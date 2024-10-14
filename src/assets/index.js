2ef10d7a35809ccc14348bef15813e5385dde5bd
Ameya <va0842@srmist.edu.in>
2024-10-14 21:27:29 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,8 @@
+import logoDark from "./logodark.png";
+import logoLight from "./logolight.png";
+import cartImg from "./cart.png";
+import paymentLogo from "./paymentlogo.png";
+import googleLogo from "./google.png";
+import githubLogo from "./github.png";
+
+export{ logoDark, logoLight, cartImg, paymentLogo, googleLogo, githubLogo };
---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDgzGigkZOJMmZwP2rvcTJ62yadIFlDaPQ",
+  authDomain: "urbaneoapp.firebaseapp.com",
+  projectId: "urbaneoapp",
+  storageBucket: "urbaneoapp.appspot.com",
+  messagingSenderId: "137666305036",
+  appId: "1:137666305036:web:c8d82eddcf661153a12229"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
